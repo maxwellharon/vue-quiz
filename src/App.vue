ed086f9a9799126dcbaf838a5a388cf9935fc402
maxwellharon <maxwellharon54@gmail.com>
2019-09-11 16:13:41 +0300

    all

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header/>
+    <QuestionBox/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import QuestionBox from './components/QuestionBox.vue'
+
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    QuestionBox
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Question Box</h1>
+</template>
